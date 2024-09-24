27c085831d1dcd197f49178966d5e73e621dffd5
MrITluvr <iturrinos07@gmail.com>
2024-09-24 14:25:04 -0400

    Add files via upload

---
+++ P1LAB1_IturrinoSamyer.py
@@ -0,0 +1,13 @@
+# Samyer Iturrino
+# September 24, 2024
+# P1LAB1
+# This code will ask for your name and return it
+
+# get the first name
+first_name = input("Enter your first name: ")
+
+# get the last name
+last_name = input ("Enter your last name: ")
+
+# return the value
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
