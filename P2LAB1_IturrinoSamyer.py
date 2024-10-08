9354b961475073a67185f2a06f750cc1016f242b
MrITluvr <iturrinos07@gmail.com>
2024-10-08 14:20:32 -0400

    Add files via upload

---
+++ P2LAB1_IturrinoSamyer.py
@@ -0,0 +1,24 @@
+# Samyer Iturrino
+# October 8 2024
+# P2LAB1
+# Using Python's built-in library
+
+# import math library
+import math
+
+
+ 
+
+# Example usage
+radius = float(input("What is the radius of the circle \n"))
+
+# Calculate
+
+diameter = 2 * radius
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+
+
+print(f"The diameter of the circle is {diameter}\n")
+print(f"The circumference of the circle is {circumference:.2f}\n")
+print(f"The area of the circle is {area:.2f}\n")
